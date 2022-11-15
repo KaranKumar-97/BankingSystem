f65fa5411abba1db9b25a2537c4e672ababf2bb7
Karan Kumar <105391077+KaranKumar-97@users.noreply.github.com>
2022-11-15 18:52:58 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,18 @@
+<div id="container">
+
+      <footer class="bg-dark text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+  
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.2); color:yellow;"><b>
+
+
+ © 2022. Made By Karan Kumar <br>
+      The Sparks Foundation</div></b>
+
+  </div>
+  <!-- Copyright -->
+ </div>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include('nav.php') ?>
+  
+  
+  <h1>The Sparks Foundation Project
+   <h1>Basic Banking System</h1> 
+  </h1>
+  <div class="bg">
+    <div class="center">
+      <ul>
+        <li class="operations"><a href="customer.php"><button class="btn" id="red">View All Users</button></a></li>
+        <li class="operations"><a href="send.php"><button class="btn" id="blue">Transfer Money</button></a></li>
+        <li class="operations"><a href="transaction.php"><button class="btn" id="yellow">View Transfer History</button></a></li>
+      </ul>
+    </div>
+  </div>
+  
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
